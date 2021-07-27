723783aa888dac3cc40f109a0c70993b97ffd2b9
boboan <38344354+WBboboan@users.noreply.github.com>
2021-07-27 16:38:46 +0800

    Init    Commit
    Init    Commit

--- src/3dmap/visulation/heatmap/heatmap.js
+++ src/3dmap/visulation/heatmap/heatmap.js
@@ -1,5 +1,3 @@
-// import * as Cesium from 'cesium';
-
 import data  from './last-all-airbox.js'
 import getCesiumHeat from './lib/CesiumHeat.js'
 
@@ -16,7 +14,6 @@ export function addHeatmap(viewer) {
                 value: s_d0,
             };
         }),
-        // bbox for heatmap | 只在范围内显示热力图拉近会清晰些，默认整个地球但拉近后巨模糊
         [120.106188593, 21.9705713974, 121.951243931, 25.2954588893]
     );
     // destory | 销毁
